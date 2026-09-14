d3d6602d6b3953732ffce7cd8ad43b174ec2ddb2
MorpheeSp <morphee.sevestre@gmail.com>
2026-09-14 15:43:49 +0200

    temporaire ajout controller

---
+++ Maths_moteur_2D/Maths_moteur_2D/Controller.cpp
@@ -0,0 +1,9 @@
+#include "Controller.h"
+#include <QKeyEvent>
+
+void Controller::handleInput()
+{
+	// Handle user input and update selected rigidbodies accordingly
+	// We're using zqsd for movement, and space for stopping
+
+}
---
+++ Maths_moteur_2D/Maths_moteur_2D/Controller.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Rigidbody.h"
+
+// Classe qui sélectionne un ou plusieurs rigidbody et contrôle leur mouvement en fonction des entrées de l'utilisateur (clavier, souris, etc.)
+class Controller
+{
+	std::vector<rigidbody*> selectedRigidbodies;
+	void handleInput(); // Gère les entrées de l'utilisateur et met à jour les rigidbody sélectionnés en conséquence
+};
+
